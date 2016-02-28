9dfb61481f6fad80cd9a76901eeb130569b1bc12
Shannon Bertucci <shannon.bertucci@gmail.com>
2016-02-27 20:32:48 -0700

    Fixes build

--- gulpfile.js
+++ gulpfile.js
@@ -4,14 +4,31 @@ var gulp = require('gulp'),
     karma = require('karma').server,
     stylish = require('jshint-stylish'),
     connect = require('gulp-connect'),
-    sass = require('gulp-sass'),
+    sass = require('gulp-sass');
     sassGlob = require('gulp-sass-glob');
+    concat = require('gulp-concat'),
+    uglify = require('gulp-uglify'),
+    imagemin = require('gulp-imagemin'),
+    dom = require('gulp-html-replace'),
+    ngHtml2js = require("gulp-ng-html2js"),
+    ngmin = require('gulp-ng-annotate'),
+    htmlmin = require('gulp-htmlmin'),
+    cssmin = require('gulp-cssmin'),
+    packagejson = require('./package.json'),
+    streamqueue = require('streamqueue'),
+    rimraf = require('rimraf'),
+    rename = require('gulp-rename'),
+    jshint = require('gulp-jshint'),
+    stylish = require('jshint-stylish'),
+    domSrc = require('gulp-dom-src');
+
 
 var MAIN_STYLE_SRC = 'app.scss';
 
 var JS_SRC = ['partial/**/*.js', 'directive/**/*.js', 'service/**/*.js', 'filter/**/*.js'];
 var STYLE_SRC = [MAIN_STYLE_SRC, 'partial/**/*.scss', 'directive/**/*.scss', 'styles/**/*.scss'];
 var TMPL_SRC = ['partial/**/*.html', 'directive/**/*.html', 'service/**/*.html', 'filter/**/*.html'];
+var DIST_DEST = 'dist/';
 
 /**
  * Run test once and exit
@@ -67,5 +84,67 @@ gulp.task('watch', function() {
   gulp.watch(STYLE_SRC, ['test', 'sass', 'html']);
   gulp.watch(JS_SRC, ['test', 'jshint', 'html']);
 });
+
+gulp.task('css', function() {
+    gulp.src(MAIN_STYLE_SRC)
+        .pipe(sassGlob())
+        .pipe(sass())
+        .pipe(cssmin({keepSpecialComments: 0}))
+        .pipe(rename('webapp.full.css'))
+        .pipe(gulp.dest(DIST_DEST+'css'));
+});
+
+gulp.task('js', function() {
+
+    var templateStream = gulp.src(TMPL_SRC)
+        .pipe(htmlmin({
+          removeComments: true,
+          conservativeCollapse: true,
+          keepClosingSlash: true,
+          preserveLineBreaks: true
+        }))
+        .pipe(ngHtml2js({
+            moduleName: packagejson.name
+        }));
+
+    var jsStream = domSrc({file:'index.html',selector:'script[data-build!="exclude"]',attribute:'src'});
+
+    var combined = streamqueue({ objectMode: true });
+
+    combined.queue(jsStream);
+    combined.queue(templateStream);
+
+    return combined.done()
+        .pipe(concat('webapp.full.js'))
+        .pipe(ngmin())
+        .pipe(uglify())
+        .pipe(gulp.dest(DIST_DEST+'js'));
+});
+
+gulp.task('indexHtml', function() {
+    return gulp.src('index.html')
+        .pipe(dom({
+            'css': {
+              src: '/css/webapp.full.css',
+              tpl: '<link rel="stylesheet" href="%s" />'
+            },
+            'js': '/js/webapp.full.js'
+        }))
+        .pipe(htmlmin({
+          removeComments: true,
+          conservativeCollapse: true,
+          keepClosingSlash: true,
+          preserveLineBreaks: true
+        }))
+        .pipe(gulp.dest(DIST_DEST));
+});
+
+gulp.task('images', function(){
+    return gulp.src('img/**')
+        .pipe(imagemin())
+        .pipe(gulp.dest(DIST_DEST+'img'));
+});
+
+gulp.task('build', ['test', ''])
 
 gulp.task('default', ['test', 'jshint', 'serve', 'watch']);
